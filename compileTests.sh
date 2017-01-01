c06191529be7e30b6286cd3322cab97bb6d6992b
LukeThoma5 <luke.4man@btinternet.com>
2017-01-01 19:27:55 +0000

    Updated tests to new infastructure

--- compileTests.sh
+++ compileTests.sh
@@ -1,3 +1,32 @@
 #! /bin/bash
 
-g++ tests.cpp word.cpp badWord.cpp masterSyllableList.cpp wordContainer.cpp badwordContainer.cpp wordCC.cpp GUI.cpp hLog.cpp logEvent.cpp -o tests `pkg-config gtkmm-3.0 --cflags --libs`
+#g++ tests.cpp word.cpp badWord.cpp masterSyllableList.cpp wordContainer.cpp badwordContainer.cpp wordCC.cpp GUI.cpp hLog.cpp logEvent.cpp -o tests `pkg-config gtkmm-3.0 --cflags --libs`
+g++ ./src/tests.cpp\
+ ./objects/word.cpp\
+ ./objects/badWord.cpp\
+ ./objects/masterSyllableListTree.cpp\
+ ./objects/masterSyllableListMap.cpp\
+ ./objects/syllableNode.cpp\
+ ./objects/wordContainer.cpp\
+ ./objects/badwordContainer.cpp\
+ ./objects/wordCC.cpp\
+ ./src/GUI.cpp\
+ ./src/GUI/GUI_SpellingScreen.cpp\
+ ./src/GUI/GUI_KeyboardScreen.cpp\
+ ./src/GUI/GUI_HangmanScreen.cpp\
+ ./src/GUI/GUI_AnalysisScreen.cpp\
+ ./src/GUI/GUI_OptionsScreen.cpp\
+ ./src/GUI/GUI_PasswordScreen.cpp\
+ ./src/GUI/GUI_ResetDataScreen.cpp\
+ ./objects/hLog.cpp\
+ ./objects/logEvent.cpp\
+ ./src/externalCode.cpp\
+ ./src/SHA256.cpp\
+ ./src/speech.cpp\
+ ./src/crypto.cpp\
+ ./src/Misc/printVector.cpp\
+ ./src/Misc/mergeSortHelpers.cpp\
+ ./src/Misc/MSLHelpers.cpp\
+ ./src/Misc/Utils.cpp\
+ -o tests\
+ `pkg-config gtkmm-3.0 --cflags --libs`
--- src/tests.cpp
+++ src/tests.cpp
@@ -6,193 +6,43 @@
 
 #include <cstdlib> //Declare system() which comes from a c library
 
-//Needed for say functin and pipe functionality
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 #include <gtkmm.h>
 
-#include "randng.h" //My random number generator header
-#include "masterSyllableList.h" //MSL declaration
-#include "word.h"
-#include "badWord.h"
-#include "SSG.h"
-#include "wordContainer.h"
-#include "badwordContainer.h"
-#include "wordCC.h"
-#include "hLog.h"
+#include "../headers/randng.h" //random number generator header
+#include "../headers/masterSyllableList.h" //MSL declaration
+#include "../headers/masterSyllableListTree.h"
+#include "../headers/masterSyllableListMap.h"
+#include "../headers/word.h"
+#include "../headers/badWord.h"
+#include "../headers/wordContainer.h"
+#include "../headers/badwordContainer.h"
+#include "../headers/wordCC.h"
+#include "../headers/hLog.h"
 
-#include "windowContainer.cpp"
+#include "../objects/windowContainer.cpp"
 
 using namespace std;
 
 namespace SSG {
 	//SSG SpellingSuiteGlobalVariables
-	masterSyllablesList MSL; //MasterSyllablesList
+	//masterSyllablesListTree physicalMSL;
+	masterSyllablesListMap physicalMSL;
+	masterSyllablesList* MSL = &physicalMSL; //MasterSyllablesList
 	wordContainer* goodWordList = NULL; //Allows the currently worked on wordList to be updated and interchanged
 	wordContainer* badWordList = NULL; //Defaulting to null prevents manipulating objects that don't exist.
 	Glib::RefPtr<Gtk::Builder> refBuilder;
 	windowContainer winContainer;
-	// wordCC SpellingWords("finalDictwithDef.txt", "wrongWords.txt");
-	wordCC SpellingWords("shortDict.txt", "shortwrongWords.txt");
-	hLog histLog("SSGHistFile.txt");
+	//wordCC SpellingWords("./Data/finalDictwithDef.txt", "./Data/wrongWords.txt");
+	wordCC SpellingWords("./Data/shortDict.txt", "./Data/shortwrongWords.txt");
+	hLog histLog("./Data/SSGHistFile.txt");
 	time_t sessionStartTime = time(0);
 }
 
-
-
-void say(const string& sentence)
-{
-	/* //Old say function
-	//string Command = "flite -voice slt -t \"" + sentence + "\"";
-	string Command = "flite -t \"" + sentence + "\"";
-
-    system(Command.c_str());
-	*/
-
-	int fd;
-    std::string myfifo = "/tmp/myfifo";
-    //char * myfifo = "/tmp/myfifo";
-
-    /* create the FIFO (named pipe) */
-    mkfifo(myfifo.c_str(), 0666);
-
-    /* write "Hi" to the FIFO */
-    fd = open(myfifo.c_str(), O_WRONLY);
-    //write(fd, argv[1], sizeof(argv[1]));
-    write(fd,sentence.c_str(),100);
-    close(fd);
-
-    /* remove the FIFO */
-    unlink(myfifo.c_str());
-}
-
-string makeUpperCase(const string& attempt)
-{
-    //Should be ok but some shortcuts were made for efficency. May cause errors
-    string upperString;
-    for (unsigned int i=0; i<attempt.size(); i++)
-    {
-        char currentChar = attempt[i];
-        int charInt = static_cast<int>(currentChar);
-        if (charInt>96)
-        {
-            charInt -= 32;
-        }
-        upperString += static_cast<char>(charInt);
-    }
-    return upperString;
-}
-
-void speak(const string& wordToSay, const bool isCorrect)
-{
-	/*
-	//string Command = "flite -voice slt -t \"Please spell the word " + wordToSay + "\"";
-	string Command = "flite -t \"Please spell the word " + wordToSay + "\"";
-    system(Command.c_str());
-	*/
-	if (isCorrect)
-		say("That is correct, please spell the word " + wordToSay);
-	else
-		say("Please spell the word " + wordToSay);
-}
-
-void printVector(const vector<string>& sV, int start=0, int stop=-1)
-{
-	if (stop==-1)
-		stop=sV.size();
-	for (unsigned int i=0; i < sV.size(); i++)
-	{
-		cout << i << ":" << sV[i] << ' ';
-	}
-	cout << endl;
-}
-
-void printVector(const vector<int>& sV, int start=0, int stop=-1)
-{
-	if (stop==-1)
-		stop=sV.size();
-	for (unsigned int i=start; i < stop; i++)
-	{
-		cout << i << ":" << sV[i] << ' ';
-	}
-	cout << endl;
-}
-
-void printVector(const vector<bool>& sV, int start=0, int stop=-1)
-{
-	if (stop==-1)
-		stop=sV.size();
-	for (unsigned int i=start; i < stop; i++)
-	{
-		cout << i << ":" << sV[i] << ' ';
-	}
-	cout << endl;
-}
-
-void createRandomWordWrongCounts() //Fill the MSL with random wrongCounts to test sorting
-{
-	const int HIGH = 12; //Was 450
-	const int LOW = 0;
-
-	for (unsigned int i=0; i<SSG::MSL.wrongCount.size(); i++)
-	{
-		SSG::MSL.wrongCount[i] = randNG(LOW,HIGH); //Valgrind does not like this call
-		//SSG::MSL.wrongCount[i] = 0; //This can be used to memory leak testing
-	}
-}
-
-int randNG(int low, int high)
-{
-	std::mt19937 rng;
-	rng.seed(std::random_device()());
-	std::uniform_int_distribution<std::mt19937::result_type> dist6(low,high);
-	return dist6(rng);
-}
-
-int letterToInt(char c)
-{
-	return static_cast<int>(c)-65;
-}
-
-word& returnRef(word* myWord)
-{
-	return *myWord;
-}
-
-void SpellingTest(wordCC& SpellingWords)
-{
-	SpellingWords.generatewScore();
-
-	SpellingWords.findHardest();
-
-	for (int i=0; i<10; i++)
-	{
-		//SpellingWords.printwordCC();
-		string wordToSpell = SpellingWords[i]->getWord();
-		speak(wordToSpell,false);
-		string userInput;
-		cin >> userInput;
-		if (userInput == "exit")
-			exit(0);
-		if (userInput != wordToSpell)
-		{
-			cout << "You spelt it: " << userInput << " correct spelling is " << wordToSpell << endl;
-			SpellingWords.wordWrong(i,userInput);
-		}
-		else
-		{
-			cout << "Correct!" << endl;
-		}
-
-	}
-}
+void createRandomWordWrongCounts();
 
 bool testWordContainerSearch(wordContainer& goodWords)
 {
-    //string.compare has issues when passed antic and anti-Federalist
+    //string.compare has issues when passed "antic" and "anti-Federalist", doesn't like the -
     //bool success = true;
     int j = 0;
     for (int i=0; i<goodWords.size(); i++)
@@ -224,12 +74,12 @@ bool testWordContainerSearch(wordContainer& goodWords)
 
 bool testMSLMap(const string& testType)
 {
-	for (int i=0; i<SSG::MSL.size(); i++) //For every syllable in the masterSyllableList
+	for (int i=0; i<SSG::MSL->size(); i++) //For every syllable in the masterSyllableList
 	{
-		string testSyllable = SSG::MSL[i];
+		string testSyllable = (*SSG::MSL)[i];
 		//cout << SSG::MSL.getSyllableWCount(i) << "  " << SSG::MSL.wrongCount[i] << '\n';
 		//cout << "Testing syllable " << testSyllable << " at position " << i << " does " << SSG::MSL.getSyllableWCount(i) << "==" << SSG::MSL.getSyllableWCount(testSyllable) << "?\n";
-		if (SSG::MSL.getSyllableWCount(i) != SSG::MSL.getSyllableWCount(testSyllable)) //If there is a mismatch in the returned wrongCount
+		if (SSG::MSL->getSyllableWCount(i) != SSG::MSL->getSyllableWCount(testSyllable)) //If there is a mismatch in the returned wrongCount
 		{
 			cout << "Error MSL position and value do mismatch!\nTest syllable " << testSyllable << " at position " << i << endl;
 			cout << testType << "[Failed]" << endl;
@@ -241,9 +91,9 @@ bool testMSLMap(const string& testType)
 
 void printMSL()
 {
-	for (int i=0; i<SSG::MSL.size(); i++)
+	for (int i=0; i<SSG::MSL->size(); i++)
 	{
-		cout << SSG::MSL[i] << '[' << SSG::MSL.getSyllableWCount(i) << " :: " << SSG::MSL.getSyllableWCount(SSG::MSL[i]) << "]\n";
+		cout << (*SSG::MSL)[i] << '[' << SSG::MSL->getSyllableWCount(i) << " :: " << SSG::MSL->getSyllableWCount((*SSG::MSL)[i]) << "]\n";
 	}
 }
 
@@ -254,7 +104,7 @@ int main(int argc, char const *argv[]) {
 	createRandomWordWrongCounts();
 	printMSL();
 	testMSLMap("MSL Map Creation Test ");
-	SSG::MSL.sortList();
+	SSG::MSL->sortList();
 	testMSLMap("Merge sorted Map Test ");
 	printMSL();
 	cout << "Tests complete" << endl;
