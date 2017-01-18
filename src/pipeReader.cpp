e446182aa98b9e829ae267a34ae41116ec7b26cc
LukeThoma5 <luke.4man@btinternet.com>
2017-01-18 21:42:22 +0000

    Made pipeReader more efficent

--- src/pipeReader.cpp
+++ src/pipeReader.cpp
@@ -6,40 +6,29 @@
 
 #include <iostream>
 
-#define MAX_BUF 4096
+const int MAX_BUF = 4096; //Declare max buffer size, must be the same on client and server
 
 int main()
 {
-    
-
     /* open, read, and display the message from the FIFO */
-    char lastbuf[MAX_BUF];
-    std::string lastInput = "";
-    std::string buffer = "";
-    while (1)
+    char buf[MAX_BUF]; //Create an array in memory to read the pipe into
+    std::string lastInput = ""; //Create a string to hold the last read info
+    std::string buffer = ""; //Creat a string to hold the current buffer to create the speack command with
+    const std::string SSGFIFO = "/tmp/SSGFIFO"; //Declare the name of the pipe
+    while (1) //While the program runs
     {
-    	int fd;
-    	std::string myfifo = "/tmp/myfifo";
-    	char buf[MAX_BUF];
-    	fd = open(myfifo.c_str(), O_RDONLY);
-    	read(fd, buf, MAX_BUF);
-    	close(fd);
-    	buffer = buf;
-    	if (buffer != lastInput)
+    	int fileDescriptor = open(SSGFIFO.c_str(), O_RDONLY); //Open the pipe in read only mode
+    	read(fileDescriptor, buf, MAX_BUF); //Read the pipe and store it in the char buffer
+    	close(fileDescriptor); //Close the pipe so that the main program can write data to it
+    	buffer = buf; //Set the buffer string to the content of the buffer
+    	if (buffer != lastInput) //If something new to say
     	{
-    		printf("Received: %sEND\n", buf);
-    		std::string command = "espeak \"";
-    		command += buf;
-    		command += '\"';
-    		std::cout << command << std::endl;
-    		system(command.c_str());
+    		printf("Received: %sEND\n", buf); //Print what has been read in to check for garbled data
+    		std::string command = "espeak \"" + buffer + '\"'; //create the command to call the speak program to say the buffer contents
+    		system(command.c_str()); //Execute the command using the system shell
+            lastInput = buffer; //Only update the lastInput if something changed
     	}
-    	
-    	lastInput = buf;
-    	sleep(0.05);
+    	sleep(0.05); //Limit pipe reading to 20Hz
     }
-    
-    
-
-    return 0;
+    return 0; //Tell OS program sucessful
 }
--- src/speech.cpp
+++ src/speech.cpp
@@ -14,15 +14,15 @@ using namespace std;
 
 void say(const string& sentence)
 {
-    std::string myfifo = "/tmp/myfifo";
+    std::string myfifo = "/tmp/SSGFIFO"; //The pipes location in the file structure
     /* create the FIFO (named pipe) */
     mkfifo(myfifo.c_str(), 0666);
 
 	/* write my message to the Pipe (FIFO) */
     int fileDescriptor = open(myfifo.c_str(), O_WRONLY);
     //write to the fileDescriptor opened above the array of char in sentence with a max size of 100
-    write(fileDescriptor,sentence.c_str(),100);
-    close(fileDescriptor);
+    write(fileDescriptor,sentence.c_str(),100); //Write a maximum of 100 characters to the buffer
+    close(fileDescriptor); //Close the pipe
 
     /* unlick the FIFO */
     unlink(myfifo.c_str());
