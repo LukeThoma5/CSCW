5bc62d4fa4405f8235dce8bed002446db4319ce2
LukeThoma5 <luke.4man@btinternet.com>
2017-01-18 20:57:23 +0000

    Added more comments to main

--- src/main.cpp
+++ src/main.cpp
@@ -32,12 +32,12 @@ namespace SSG {
 	extern const time_t sessionStartTime = time(0); //Set a constant that has external linkage (can be seen by other translation units) to the time at startup of the program
 }
 
-void createRandomWordWrongCounts();
-void connectSignals();
+void createRandomWordWrongCounts(); //Defined in src/Misc/MSLHelpers
+void connectSignals(); //Defined in src/GUI.cpp
 
 int main (int argc, char **argv)
 {
-	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example"); //Create a gtk Application, pass the argv to gtk so it can extract the relevant options eg gtk themes
 
     //Load the GtkBuilder file and instantiate its widgets:
     SSG::refBuilder = Gtk::Builder::create();
@@ -47,29 +47,33 @@ int main (int argc, char **argv)
     }
     catch(const Glib::FileError& ex)
     {
+	  //File not found
       std::cerr << "FileError: " << ex.what() << std::endl;
       return 1;
     }
     catch(const Glib::MarkupError& ex)
     {
+	  //Error in the file structure
       std::cerr << "MarkupError: " << ex.what() << std::endl;
       return 1;
     }
     catch(const Gtk::BuilderError& ex)
     {
+	  //Error in the file (wrongly defined item)
       std::cerr << "BuilderError: " << ex.what() << std::endl;
       return 1;
     }
 
+	//Add the windows from the Gtk::builder
 	SSG::winContainer.addWindows(SSG::refBuilder);
-	connectSignals();
+	connectSignals(); //Add the signal handlers to the signals that may be created during program execution
 
-	if (SSG::MSL->hasNoValues())
-		createRandomWordWrongCounts();
+	if (SSG::MSL->hasNoValues()) //If the first run eg no wrong counts loaded
+		createRandomWordWrongCounts(); //Set up some randomly until the user gets some wrong
 
-	app->run(*(SSG::winContainer.MainScreen));
+	app->run(*(SSG::winContainer.MainScreen)); //Run the GUI, when the user closes main window continue execution in main
 
-	cout << "Returning to OS" << endl;
+	cout << "Returning to OS" << endl; //Message to determine object destruction about to occur
 
-	return 0;
+	return 0; //Give a success signal to the operating system
 }
