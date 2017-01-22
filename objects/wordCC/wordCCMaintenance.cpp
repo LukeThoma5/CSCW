4211b765e0e8a5e2e6298a9fc873f60ac8631894
LukeThoma5 <luke.4man@btinternet.com>
2017-01-22 11:08:36 +0000

    Commented wordCC Maintenance functions

--- objects/wordCC/wordCCMaintenance.cpp
+++ objects/wordCC/wordCCMaintenance.cpp
@@ -18,25 +18,13 @@ namespace SSG {
 
 using namespace std;
 
-void printVector(const vector<string>& sV, int start=0, int stop=-1); //Early declaration
-void printVector(const std::vector<int>& sV, int start=0, int stop=-1);
-void printVector(const std::vector<bool>& sV, int start=0, int stop=-1);
-void speak(const string& wordToSay, const bool isCorrect);
-void say(const string& sentence);
-string seperateWord(const string& wordToSep);
-string makeUpperCase(const string& attempt);
-
 void wordCC::clearWordPos()
 {
     wordPos.clear(); //Removes previous positions
     int totalSize = goodWords.size() + badWords.size();
     wordPos.reserve(totalSize); //Reduces the need to move the vector in memory by saying out the gate the miniumum space needed
-    goodBadPos.clear();
-    goodBadPos.reserve(totalSize);
-    //for (unsigned int i=0; i<totalSize; i++)
-    //{
-    //    wordPos.push_back(0);
-    //}
+    goodBadPos.clear(); //Remove good/bad indicators
+    goodBadPos.reserve(totalSize); //Reduce mem allocs
 }
 
 void wordCC::removeDuplicates(const vector<string>& dupWords)
@@ -47,31 +35,33 @@ void wordCC::removeDuplicates(const vector<string>& dupWords)
 		int dupLocation;
 		try {dupLocation = goodWords.binSearch(dupWords[i],0,goodWords.size());}
 		catch (int errorNum)
+		//If the word could not be found in the goodWords container, it is not duplicated. Don't try and remove again
 		{
 			dupLocation = -1;
 			cout << "Failed to find word to delete, binSearch returned with code " << errorNum << endl;
 		}
         cout << "Duplicate word " << dupWords[i] << " found in word container at location " << dupLocation << endl;
         //Delete the located word
-        if (dupLocation != -1)
-            goodWords.deleteWord(dupLocation);
+        if (dupLocation != -1) //If word found
+            goodWords.deleteWord(dupLocation); //Tell goodWords to destruct word and fix its abstraction
     }
 }
 
 void wordCC::fixwordPos(const int& goodWordLocation)
 {
-    for (int i=0; i<wordPos.size(); i++)
+    for (int i=0, end=wordPos.size(); i<end; ++i)
     {
         if (goodBadPos[i] == false)
+		//If a good word
         {
-            if (wordPos[i] > goodWordLocation)
-                wordPos[i]--;
+            if (wordPos[i] > goodWordLocation) //If later in the alphabet than the word that was removed
+                wordPos[i]--; //Shift it back by one
         }
     }
 }
 
 void wordCC::addWord(word* wordToAdd)
 {
-    //Add code to fix wordCC not wordContainer abstraction?
     goodWords.addWord(wordToAdd);
+	//The word is added in the alphabetical order in the goodWords list but at the end of its abstraction so that is unaffected. The word is inassessible to the outside until a call is made to findHardest (findSpellingWords or findKeyboardWords)
 }
