63354ff5ceee47df6dd67992d2365ccbc0eaa576
LukeThoma5 <luke.4man@btinternet.com>
2017-01-23 22:01:11 +0000

    Comment keyboard graphs

--- objects/hLog/hLogKeyboardGraphs.cpp
+++ objects/hLog/hLogKeyboardGraphs.cpp
@@ -10,24 +10,25 @@ namespace SSG {
 }
 
 void hLog::graphKeyboard(std::time_t startPoint, int eventItem, const string& graphName, const std::string& filename)
+//Keyboard graph creator due to frequency of keyboard graphs
 {
-    vector<logEvent*> keyboardComplete;
-    getEventPointers(startPoint, keyboardComplete, "keyboardComplete");
-    if (keyboardComplete.size())
+    vector<logEvent*> keyboardComplete; //Log event pointers container
+    getEventPointers(startPoint, keyboardComplete, "keyboardComplete"); //Get keyboardComplete event pointers
+    if (keyboardComplete.size()) //If atleast 1 event
     {
-        vector<float> xcoords;
-        vector<float> ycoords;
-        for (int i=0; i<keyboardComplete.size(); i++)
+        vector<float> xcoords; //X values
+        vector<float> ycoords; //Y values
+        for (int i=0, keyboardCompleteSize=keyboardComplete.size(); i<keyboardCompleteSize; ++i)
         {
-            vector<string> dataItems = keyboardComplete[i]->getDataItems();
-            xcoords.push_back(keyboardComplete[i]->getTime());
-            ycoords.push_back(stof(keyboardComplete[i]->getDataItems()[eventItem]));
+            xcoords.push_back(keyboardComplete[i]->getTime()); //Add the timestamp
+            ycoords.push_back(stof(keyboardComplete[i]->getDataItems()[eventItem])); //Add the data item choosen by paramter
         }
-        createScatterGraph(graphName,xcoords,ycoords,filename);
+        createScatterGraph(graphName,xcoords,ycoords,filename); //Pass the paramters to the createScatterGraph private function
     }
 }
 
 void hLog::graphKeyboardMovingAvg(std::time_t startPoint, int eventItem, const string& graphName, const std::string& filename)
+//Most coments identical to above, changes commented
 {
     vector<logEvent*> keyboardComplete;
     getEventPointers(startPoint, keyboardComplete, "keyboardComplete");
@@ -35,19 +36,20 @@ void hLog::graphKeyboardMovingAvg(std::time_t startPoint, int eventItem, const s
     {
         vector<float> xcoords;
         vector<float> ycoords;
-        for (int i=0; i<keyboardComplete.size(); i++)
+        for (int i=0; i<keyboardComplete.size(); ++i)
+		//For every event from the startPoint onwards
         {
             vector<logEvent*> itemsToAvg;
-            getWeekEventPointers(keyboardComplete[i]->getTime(), itemsToAvg, "keyboardComplete");
+            getWeekEventPointers(keyboardComplete[i]->getTime(), itemsToAvg, "keyboardComplete"); //Get the pointers on a week around the current pointer
             float itemTotal = 0;
-            for (int j=0; j<itemsToAvg.size(); j++)
+            for (int j=0; j<itemsToAvg.size(); j++) //For every pointer in the week to average
             {
-                itemTotal += stof(itemsToAvg[j]->getDataItems()[eventItem]);
+                itemTotal += stof(itemsToAvg[j]->getDataItems()[eventItem]); //Add the dataItems to the total
             }
 
-            xcoords.push_back(keyboardComplete[i]->getTime());
-            ycoords.push_back(itemTotal/float(itemsToAvg.size()));
+            xcoords.push_back(keyboardComplete[i]->getTime()); //Add the timestamp
+            ycoords.push_back(itemTotal/float(itemsToAvg.size())); //Add the average of the pointer dataItems
         }
-        createScatterGraph(graphName,xcoords,ycoords,filename);
+        createScatterGraph(graphName,xcoords,ycoords,filename); //Create the graph
     }
 }
